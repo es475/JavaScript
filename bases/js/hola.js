6722fee4183814fdbf22f9f25d3fa259d2cc8e76
es475 <halo.3.a@hotmail.es>
2022-12-01 16:33:46 -0600

    Bases de javaScript/ base / #1

---
+++ bases/js/hola.js
@@ -0,0 +1,5 @@
+alert("Hola Mundo con Js");
+alert("Bienvenido al curso de JS");
+  document.write("Hola mundo desde fichero externo");
+  console.log(84+4);
+  console.log("Muestra esto en la consola");
---
+++ bases/variables.js
@@ -0,0 +1,11 @@
+/* Variables
+ Es un contenedor de información.
+*/
+
+var pais = 'Mexico';
+var continente = "America";
+var antiguedad = 2005;
+
+
+console.log(pais, continente, antiguedad);
+
