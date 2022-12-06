967977a0ba383526619d2799a1b8dc6240896f90
es475 <halo.3.a@hotmail.es>
2022-12-06 15:45:33 -0600

    parte del dom

--- bases/dom/dom.js
+++ bases/dom/dom.js
@@ -26,14 +26,36 @@ content.style.background = 'red';
 
 console.log(content);
 
-
-todosLosDiv.forEach((element, indice) => {
-    var parrafo = document.createElement('p');
-    var texto = document.createTextNode(element);
-    parrafo.appendChild(texto);
-});
+var seccion = document.querySelector("#miseccion");
+var hr = document.createElement("hr");
 
 
+var valor;
+for(valor in todosLosDiv) {
+    if(typeof todosLosDiv[valor].textContent == 'string'){
+        var parrafo = document.createElement("p");
+        var texto = document.createTextNode(todosLosDiv[valor].textContent);
+        parrafo.append(texto);
+        seccion.append(parrafo);
+    }
+};
+seccion.append(hr);
 
 //como conseguir elementos por sus clase css
+var divs = document.getElementsByClassName('rojo');
+var divsA = document.getElementsByClassName('amarillo');
+divsA[0].style.background = "blue";
+console.log(divsA)
+var color;
+for(color of divs){
+    color.style.background = 'green';
 
+}
+console.log(divs);
+
+//queryselector
+var id = document.querySelector('#encabezado');
+console.log(id);
+
+var claseR = document.querySelector('.rojo');
+console.log(claseR);
